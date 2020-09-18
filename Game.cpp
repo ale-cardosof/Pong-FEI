f313d1b7cf1f579be018a6fd34835079a3604d26
Alexandre Cardoso <37407969+ale-cardosof@users.noreply.github.com>
2020-09-18 00:30:33 -0300

    Adicionamos a Segunda Raquete + colisões + identificação de gol
    Adicionamos a Segunda Raquete, colisão com a segunda raquete e a movimentação da segunda raquete sendo manipulada pela máquina.
    Além disso, refizemos as colisões com as laterais e alteramos o tamanho da janela.
    Também criamos as variáveis que irão armazenar o placar.

--- Game.cpp
+++ Game.cpp
@@ -7,13 +7,16 @@
 // ----------------------------------------------------------------
 
 #include "Game.h"
+#include <iostream>
 
 const int thickness = 15;
 const float paddleH = 100.0f;//tamanho da raquete
+int golsEsq = 0;
+int golsDir = 0;
 
 Game::Game()
 	:mWindow(nullptr)//para criar uma janela
-	, mRenderer(nullptr)//para fins de renderizaÁ„o na tela
+	, mRenderer(nullptr)//para fins de renderiza√ß√£o na tela
 	, mTicksCount(0)//para guardar o tempo decorrido no jogo
 	, mIsRunning(true)//verificar se o jogo ainda deve continuar sendo executado
 	, mPaddleDir(0)
@@ -36,8 +39,8 @@ bool Game::Initialize()
 		"Game Programming in C++ (Chapter 1)", // Window title
 		100,	// Top left x-coordinate of window
 		100,	// Top left y-coordinate of window
-		1024,	// Width of window
-		768,	// Height of window
+		1000,	// Width of window
+		500,	// Height of window
 		0		// Flags (0 for no flags set)
 	);
 
@@ -60,18 +63,21 @@ bool Game::Initialize()
 		return false;
 	}
 
-	mPaddlePos.x = 10.0f;//posiÁ„o inicial da raquete eixo x
-	mPaddlePos.y = 768.0f / 2.0f;//posiÁ„o inicial da raquee eixo y
+	mPaddlePos.x = 10.0f;//posi√ß√£o inicial da raquete eixo x
+	mPaddlePos.y = 490.0f / 2.0f;//posi√ß√£o inicial da raquee eixo y
 
-	mBallPos.x = 1024.0f / 2.0f;//posiÁ„o da bola 1 eixo x
-	mBallPos.y = 768.0f / 2.0f;//posiÁ„o da bola 1 eixo y
-	mBallVel.x = -200.0f;//velocidade de movimentaÁ„o da bola 1no eixo x
-	mBallVel.y = 235.0f;//velocidade de movimentaÁ„o da bola 1 no eixo y
+	mPaddlePos2.x = 980.0f;//posi√ß√£o inicial da raquete eixo x
+	mPaddlePos2.y = 490.0f / 2.0f;//posi√ß√£o inicial da raquee eixo y
 
-	mBallPos2.x = 1000.0f / 2.0f;//posiÁ„o da bola 2 eixo x
-	mBallPos2.y = 800.0f / 2.0f;//posiÁ„o da bola 2 eixo y
-	mBallVel2.x = -100.0f;//velocidade de movimentaÁ„o da bola 2 no eixo x
-	mBallVel2.y = 150.0f;//velocidade de movimentaÁ„o da bola 2 no eixo y
+	mBallPos.x = 998.0f / 2.0f;//posi√ß√£o da bola 1 eixo x
+	mBallPos.y = 502.0f / 2.0f;//posi√ß√£o da bola 1 eixo y
+	mBallVel.x = -200.0f;//velocidade de movimenta√ß√£o da bola 1no eixo x
+	mBallVel.y = 235.0f;//velocidade de movimenta√ß√£o da bola 1 no eixo y
+
+	mBallPos2.x = 1002.0f / 2.0f;//posi√ß√£o da bola 2 eixo x
+	mBallPos2.y = 498.0f / 2.0f;//posi√ß√£o da bola 2 eixo y
+	mBallVel2.x = -200.0f;//velocidade de movimenta√ß√£o da bola 1no eixo x
+	mBallVel2.y = 150.0f;//velocidade de movimenta√ß√£o da bola 2 no eixo y
 	return true;
 }
 
@@ -87,7 +93,7 @@ void Game::RunLoop()
 
 void Game::ProcessInput()
 {
-	SDL_Event event;//evento, inputs do jogador s„o armazenados aqui
+	SDL_Event event;//evento, inputs do jogador s√£o armazenados aqui
 	while (SDL_PollEvent(&event))
 	{
 		switch (event.type)
@@ -99,7 +105,7 @@ void Game::ProcessInput()
 		}
 	}
 
-	// Get state of keyboard - podemos buscar por alguma tecla especÌfica pressionada no teclado, nesse caso, Escape
+	// Get state of keyboard - podemos buscar por alguma tecla espec√≠fica pressionada no teclado, nesse caso, Escape
 	const Uint8* state = SDL_GetKeyboardState(NULL);
 	// If escape is pressed, also end loop
 	if (state[SDL_SCANCODE_ESCAPE])
@@ -107,7 +113,7 @@ void Game::ProcessInput()
 		mIsRunning = false;
 	}
 
-	// Update paddle direction based on W/S keys - atualize a direÁ„o da raquete com base na entrada do jogador
+	// Update paddle direction based on W/S keys - atualize a dire√ß√£o da raquete com base na entrada do jogador
 	// W -> move a raquete para cima, Y -> move a raquete para baixo
 	mPaddleDir = 0;
 	if (state[SDL_SCANCODE_W])
@@ -118,6 +124,29 @@ void Game::ProcessInput()
 	{
 		mPaddleDir += 1;
 	}
+
+	mPaddleDir2 = 0;
+
+	if (mBallPos.x > mBallPos2.x) {
+		if (mBallPos.y > mPaddlePos2.y)
+		{
+			mPaddleDir2 += 1;
+		}
+		else if (mBallPos.y < mPaddlePos2.y)
+		{
+			mPaddleDir2 -= 1;
+		}
+	}
+	else {
+		if (mBallPos2.y > mPaddlePos2.y)
+		{
+			mPaddleDir2 += 1;
+		}
+		else if (mBallPos2.y < mPaddlePos2.y)
+		{
+			mPaddleDir2 -= 1;
+		}
+	}
 }
 
 void Game::UpdateGame()
@@ -127,10 +156,10 @@ void Game::UpdateGame()
 		;
 
 	// Delta time is the difference in ticks from last frame
-	// (converted to seconds) - calcula o delta time para atualizaÁ„o do jogo
+	// (converted to seconds) - calcula o delta time para atualiza√ß√£o do jogo
 	float deltaTime = (SDL_GetTicks() - mTicksCount) / 1000.0f;
 
-	// Clamp maximum delta time value - valores m·ximos de delta time
+	// Clamp maximum delta time value - valores m√°ximos de delta time
 	if (deltaTime > 0.05f)
 	{
 		deltaTime = 0.05f;
@@ -139,7 +168,7 @@ void Game::UpdateGame()
 	// Update tick counts (for next frame) - atualize o tempo corrente do jogo
 	mTicksCount = SDL_GetTicks();
 
-	// Update paddle position based on direction - atualiza a posiÁ„o da raquete
+	// Update paddle position based on direction - atualiza a posi√ß√£o da raquete
 	if (mPaddleDir != 0)
 	{
 		mPaddlePos.y += mPaddleDir * 300.0f * deltaTime;
@@ -148,27 +177,41 @@ void Game::UpdateGame()
 		{
 			mPaddlePos.y = paddleH / 2.0f + thickness;
 		}
-		else if (mPaddlePos.y > (768.0f - paddleH / 2.0f - thickness))
+		else if (mPaddlePos.y > (515.0f - paddleH / 2.0f - thickness))
 		{
-			mPaddlePos.y = 768.0f - paddleH / 2.0f - thickness;
+			mPaddlePos.y = 515.0f - paddleH / 2.0f - thickness;
 		}
 	}
 
-	// Update ball position based on ball velocity - atualiza a posiÁ„o da bola com base na sua velocidade
+	if (mPaddleDir2 != 0)
+	{
+		mPaddlePos2.y += mPaddleDir2 * 300.0f * deltaTime;
+		// Make sure paddle doesn't move off screen!
+		if (mPaddlePos2.y < (paddleH / 2.0f + thickness))
+		{
+			mPaddlePos2.y = paddleH / 2.0f + thickness;
+		}
+		else if (mPaddlePos2.y > (500.0f - paddleH / 2.0f - thickness))
+		{
+			mPaddlePos2.y = 500.0f - paddleH / 2.0f - thickness;
+		}
+	}
+
+	// Update ball position based on ball velocity - atualiza a posi√ß√£o da bola com base na sua velocidade
 	mBallPos.x += mBallVel.x * deltaTime;
 	mBallPos.y += mBallVel.y * deltaTime;
 
 	mBallPos2.x += mBallVel2.x * deltaTime;
 	mBallPos2.y += mBallVel2.y * deltaTime;
 
-	//Atuliza a posiÁ„o da bola se ela colidiu com a raquete
+	//Atuliza a posi√ß√£o da bola se ela colidiu com a raquete
 	// Bounce if needed
 	// Did we intersect with the paddle?
 	float diff = mPaddlePos.y - mBallPos.y;
 
 	float diff2 = mPaddlePos.y - mBallPos2.y;
 	// Take absolute value of difference
-	//verifica se a bola 1 est· na area da raquete e na mesma posiÁ„o no eixo x
+	//verifica se a bola 1 est√° na area da raquete e na mesma posi√ß√£o no eixo x
 	diff = (diff > 0.0f) ? diff : -diff;
 	diff2 = (diff2 > 0.0f) ? diff2 : -diff2;
 	if (
@@ -193,71 +236,112 @@ void Game::UpdateGame()
 		mBallVel2.x *= -1.0f;
 	}
 
-	//Verifica se a bola saiu da tela (no lado esquerdo, onde È permitido)
+	float diff3 = mPaddlePos2.y - mBallPos.y;
+
+	float diff4 = mPaddlePos2.y - mBallPos2.y;
+	// Take absolute value of difference
+	//verifica se a bola 1 est√° na area da raquete e na mesma posi√ß√£o no eixo x
+	diff3 = (diff3 > 0.0f) ? diff3 : -diff3;
+	diff4 = (diff4 > 0.0f) ? diff4 : -diff4;
+	if (
+		// Our y-difference is small enough
+		diff3 <= paddleH / 2.0f &&
+		// We are in the correct x-position
+		mBallPos.x >= 980.0f && mBallPos.x <= 990.0f &&
+		// The ball is moving to the left
+		mBallVel.x > 0.0f)
+	{
+		mBallVel.x *= -1.0f;
+	}
+
+	if (
+		// Our y-difference is small enough
+		diff4 <= paddleH / 2.0f &&
+		// We are in the correct x-position
+		mBallPos2.x >= 980.0f && mBallPos2.x <= 990.0f &&
+		// The ball is moving to the left
+		mBallVel2.x > 0.0f)
+	{
+		mBallVel2.x *= -1.0f;
+	}
+
+
+	//Verifica se a bola saiu da tela (no lado esquerdo, onde √© permitido)
 	//Se sim, encerra o jogo
 	// Did the ball go off the screen? (if so, end game)
 	else if (mBallPos.x <= 0.0f || mBallPos2.x <= 0.0f)
 	{
-		//Paliativo para n„o terminar o jogo.
-		mBallVel.x *= -1.0f;
+		golsDir += 1;
+		if (mBallPos.x <= 0.0f) {
+			// Faz bola voltar pro meio
+			mBallPos.x = 998.0f / 2.0f;//posi√ß√£o da bola 1 eixo x
+			mBallPos.y = 502.0f / 2.0f;//posi√ß√£o da bola 1 eixo y
+			mBallVel.x = -200.0f;//velocidade de movimenta√ß√£o da bola 1no eixo x
+			mBallVel.y = 235.0f;//velocidade de movimenta√ß√£o da bola 1 no eixo y
+			mBallVel.x *= -1.0f;
+		}
+		else if (mBallPos2.x <= 0.0f) {
+			// Faz bola voltar pro meio
+			mBallPos2.x = 1002.0f / 2.0f;//posi√ß√£o da bola 2 eixo x
+			mBallPos2.y = 498.0f / 2.0f;//posi√ß√£o da bola 2 eixo y
+			mBallVel2.x = -100.0f;//velocidade de movimenta√ß√£o da bola 2 no eixo x
+			mBallVel2.y = 150.0f;//velocidade de movimenta√ß√£o da bola 2 no eixo y
+			mBallVel2.x *= -1.0f;
+		}
 		//mIsRunning = false;
 	}
 
 	//Atulize a velocidade da bola se ela colidir com a parede do lado direito
 	// Did the ball collide with the right wall?
-	else if (mBallPos.x >= (1024.0f - thickness) && mBallVel.x > 0.0f)
+	else if ((mBallPos.x >= (1000.0f - thickness) && mBallVel.x > 0.0f)  || (mBallPos2.x >= (1000.0f - thickness) && mBallPos2.x > 0.0f))
 	{
-		mBallVel.x *= -1.0f;
+		golsEsq += 1;
+		if (mBallPos.x >= (1000.0f - thickness) && mBallVel.x > 0.0f) {
+			// Faz bola voltar pro meio
+			mBallPos.x = 998.0f / 2.0f;//posi√ß√£o da bola 1 eixo x
+			mBallPos.y = 502.0f / 2.0f;//posi√ß√£o da bola 1 eixo y
+			mBallVel.x = -200.0f;//velocidade de movimenta√ß√£o da bola 1no eixo x
+			mBallVel.y = 235.0f;//velocidade de movimenta√ß√£o da bola 1 no eixo y
+			mBallVel.x *= 1.0f;
+		}
+		else if (mBallPos2.x >= (1000.0f - thickness) && mBallPos2.x > 0.0f) {
+			// Faz bola voltar pro meio
+			mBallPos2.x = 1002.0f / 2.0f;//posi√ß√£o da bola 2 eixo x
+			mBallPos2.y = 498.0f / 2.0f;//posi√ß√£o da bola 2 eixo y
+			mBallVel2.x = -100.0f;//velocidade de movimenta√ß√£o da bola 2 no eixo x
+			mBallVel2.y = 150.0f;//velocidade de movimenta√ß√£o da bola 2 no eixo y
+			mBallVel2.x *= 1.0f;
+		}
 	}
 
-	//Atualize a posiÁ„o da bola se ela colidir com a parede de cima
+	//Atualize as posi√ß√µes das bolas se ela colidir com a parede de cima
 	// Did the ball collide with the top wall?
-	if (mBallPos.y <= thickness && mBallVel.y < 0.0f)
+	if ((mBallPos.y <= thickness && mBallVel.y < 0.0f) || (mBallPos2.y <= thickness && mBallVel2.y < 0.0f))
 	{
-		mBallVel.y *= -1;
+		if (mBallPos.y <= thickness && mBallVel.y < 0.0f) {
+			mBallVel.y *= -1;
+		}
+		else if (mBallPos2.y <= thickness && mBallVel2.y < 0.0f) {
+			mBallVel2.y *= -1;
+		}
 	}
 
-	//Atualize a posiÁ„o da bola se ela colidiu com a parede de baixo
+	//Atualize as posi√ß√µes da bolas se ela colidiu com a parede de baixo
 	// Did the ball collide with the bottom wall?
-	else if (mBallPos.y >= (768 - thickness) &&
-		mBallVel.y > 0.0f)
+	else if ((mBallPos.y >= (500 - thickness) && mBallVel.y > 0.0f) || (mBallPos2.y >= (500 - thickness) && mBallVel2.y > 0.0f))
 	{
-		mBallVel.y *= -1;
+		if ((mBallPos.y >= (500 - thickness) && mBallVel.y > 0.0f)) {
+			mBallVel.y *= -1;
+		}
+		else if ((mBallPos2.y >= (500 - thickness) && mBallVel2.y > 0.0f)) {
+			mBallVel2.y *= -1;
+		}	
 	}
 
-	//Verifica se a bola 2 saiu da tela (no lado esquerdo, onde È permitido)
-	//Se sim, encerra o jogo
-	// Did the ball go off the screen? (if so, end game)
-	else if (mBallPos2.x <= 0.0f || mBallPos2.x <= 0.0f)
-	{
-		//Paliativo para n„o terminar o jogo.
-		mBallVel2.x *= -1.0f;
-		//mIsRunning = false;
-	}
+	using namespace std;
+	
+	//Colis√µes bola
 
-	//Atulize a velocidade da bola 2 se ela colidir com a parede do lado direito
-	// Did the ball collide with the right wall?
-	else if (mBallPos2.x >= (1024.0f - thickness) && mBallVel2.x > 0.0f)
-	{
-		mBallVel2.x *= -1.0f;
-	}
-
-	//Atualize a posiÁ„o da bola 2 se ela colidir com a parede de cima
-	// Did the ball collide with the top wall?
-	if (mBallPos2.y <= thickness && mBallVel2.y < 0.0f)
-	{
-		mBallVel2.y *= -1;
-	}
-
-	//Atualize a posiÁ„o da bola 2 se ela colidiu com a parede de baixo
-	// Did the ball collide with the bottom wall?
-	else if (mBallPos2.y >= (768 - thickness) &&
-		mBallVel2.y > 0.0f)
-	{
-		mBallVel2.y *= -1;
-	}
-
-	//Colisıes bola
 }
 
 //Desenhando a tela do jogo
@@ -266,8 +350,8 @@ void Game::GenerateOutput()
 	// Set draw color to blue - Setamos a cor original para o back buffer
 	SDL_SetRenderDrawColor(
 		mRenderer,
-		255,		// R
-		0,		// G 
+		0,		// R
+		112,		// G 
 		0,	// B
 		0		// A
 	);
@@ -276,34 +360,29 @@ void Game::GenerateOutput()
 	SDL_RenderClear(mRenderer);
 
 	// Draw walls - mudamos a cor de mRenderer para o desenho dos retangulos que formaram as paredes
-	SDL_SetRenderDrawColor(mRenderer, 255, 255, 255, 255);
+	SDL_SetRenderDrawColor(mRenderer, 255, 255, 0, 0);
 
 	// Draw top wall - desenhando um retangulo no topo da tela, coordenada x e y = 0, 0 representa o topo esquerdo
 	//primeiro definimos as coordenadas e tamanhos do triangulo
 	SDL_Rect wall{
 		0,			// Top left x
 		0,			// Top left y
-		1024,		// Width
+		1000,		// Width
 		thickness	// Height
 	};
 	SDL_RenderFillRect(mRenderer, &wall);//finalmente, desenhamos um retangulo no topo
 
 	//desenhamos as outras paredes apenas mudando as coordenadas de wall
 	// Draw bottom wall
-	wall.y = 768 - thickness;
+	wall.y = 500 - thickness;
 	SDL_RenderFillRect(mRenderer, &wall);
 
-	// Draw right wall
-	wall.x = 1024 - thickness;
-	wall.y = 0;
-	wall.w = thickness;
-	wall.h = 1024;
-	SDL_RenderFillRect(mRenderer, &wall);
 
-	//como as posiÁıes da raquete e da boal ser„o atualizadas a cada iteraÁ„o do game loop, criamos "membros" na classe
+
+	//como as posi√ß√µes da raquete e da boal ser√£o atualizadas a cada itera√ß√£o do game loop, criamos "membros" na classe
 	//Game.h para tal
 
-	//desenhando a raquete - usando mPaddlePos que È uma struc de coordenada que foi definida em Game.h
+	//desenhando a raquete - usando mPaddlePos que √© uma struc de coordenada que foi definida em Game.h
 	// Draw paddle
 	SDL_Rect paddle{
 		static_cast<int>(mPaddlePos.x),//static_cast converte de float para inteiros, pois SDL_Rect trabalha com inteiros
@@ -313,7 +392,15 @@ void Game::GenerateOutput()
 	};
 	SDL_RenderFillRect(mRenderer, &paddle);
 
-	//desenhando a bola - usando mBallPos que È uma struc de coordenadas definida como membro em Game.h
+	SDL_Rect paddle2{
+		static_cast<int>(mPaddlePos2.x),//static_cast converte de float para inteiros, pois SDL_Rect trabalha com inteiros
+		static_cast<int>(mPaddlePos2.y - paddleH / 2),
+		thickness,
+		static_cast<int>(paddleH)
+	};
+	SDL_RenderFillRect(mRenderer, &paddle2);
+
+	//desenhando a bola - usando mBallPos que √© uma struc de coordenadas definida como membro em Game.h
 	// Draw ball
 	SDL_Rect ball1{
 		static_cast<int>(mBallPos.x - thickness / 2),
@@ -330,6 +417,8 @@ void Game::GenerateOutput()
 	thickness,
 	thickness
 	};
+
+	// Atualiza√ß√£o do Placar
 
 	SDL_RenderFillRect(mRenderer, &ball2);
 	// Swap front buffer and back buffer
